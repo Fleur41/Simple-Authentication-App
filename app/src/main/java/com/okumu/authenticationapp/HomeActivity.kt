4ba3ee7683203bf6ae6f66a565380ec1f7b395df
Fleur41 <Muriithikings456@gmail.com>
2024-05-13 17:56:37 +0300

    Auth App

---
+++ app/src/main/java/com/okumu/authenticationapp/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.okumu.authenticationapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.okumu.authenticationapp.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
